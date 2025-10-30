8dc16270a4e8c9dabf84bbcb559cf195bf5a2dba
Boston Essary <bostoncessary@gmail.com>
2025-10-29 18:39:54 -0700

    add order

--- db/migrate/20251030012214_create_plan_exercises.rb
+++ db/migrate/20251030012214_create_plan_exercises.rb
@@ -3,6 +3,7 @@ class CreatePlanExercises < ActiveRecord::Migration[8.1]
     create_table :plan_exercises do |t|
       t.integer :reps
       t.integer :sets
+      t.integer :order
       t.references :plan, null: false, foreign_key: true
       t.references :exercise, null: false, foreign_key: true
 
