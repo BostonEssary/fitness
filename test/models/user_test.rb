27e171e2ad6ff2390fb0b0180a69319f4cd84f1e
Boston Essary <bostoncessary@gmail.com>
2025-11-09 17:56:15 -0700

    Update fixture files and tests for entries and workouts
    - Rename entry fixtures for consistency from 'entry_one' and 'entry_two' to 'one' and 'two'.
    - Add new fixture for plan enrollments to support user and plan associations.
    - Enhance workouts fixture with an 'inactive' status for workouts linked to plan enrollments.
    - Update entry tests to reflect the new fixture naming convention.
    - Add tests for active and past workouts in the User model.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,14 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(email_address: " DOWNCASED@EXAMPLE.COM ")
     assert_equal("downcased@example.com", user.email_address)
   end
+
+  test "active_workouts" do
+    user = users(:one)
+    assert_equal(user.active_workouts.count, 1)
+  end
+
+  test "past_workouts" do
+    user = users(:one)
+    assert_equal(user.past_workouts.count, 1)
+  end
 end
