ad066d1dd712481aada80a3ff95673700e83d3b6
Boston Essary <bostoncessary@gmail.com>
2025-11-09 18:36:34 -0700

    Update entry fixture and enhance Workout tests
    - Change the number of sets for the 'one' entry in the entries fixture from 1 to 2.
    - Add new tests in WorkoutTest for completed sets by entry, completed set count, and set count for entry to improve test coverage and validate functionality.

--- test/models/workout_test.rb
+++ test/models/workout_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class WorkoutTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "completed_sets_by_entry" do
+    workout = workouts(:one)
+    assert_equal(workout.completed_sets_by_entry, { entries(:one).id => [completed_sets(:one)] })
+  end
+
+  test "completed_set_count_for_entry" do
+    workout = workouts(:one)
+    assert_equal(workout.completed_set_count_for_entry(entries(:one)), 1)
+  end
+
+  test "set_count_for_entry" do
+    workout = workouts(:one)
+    assert_equal(workout.set_count_for_entry(entries(:one)), 2)
+  end
 end
