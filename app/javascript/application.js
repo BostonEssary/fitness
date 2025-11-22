a4dde49fbd8418dedcc66906ff69fd7a7472f378
Boston Essary <bostoncessary@gmail.com>
2025-11-21 22:42:12 -0700

    Implement view transitions for completed sets
    - Add CSS animations for sliding transitions when rendering completed sets.
    - Update JavaScript to handle Turbo frame rendering with view transitions.
    - Modify completed sets views to include data attributes for triggering transitions.
    - Include the turbo-view-transitions library for enhanced transition capabilities.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,39 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "@rails/request.js"
+import { shouldPerformTransition, performTransition } from "turbo-view-transitions";
+
+document.addEventListener("turbo:before-frame-render", (event) => {
+  if (shouldPerformTransition()) {
+    const defaultRender = event.detail.render;
+
+    event.detail.render = (currentFrame, newFrame) => {
+       performTransition(
+        currentFrame, 
+        newFrame,
+        async () => {
+          await defaultRender(currentFrame, newFrame);
+        }
+      );
+    };
+  }
+});
+
+// document.addEventListener("turbo:before-frame-render", (event) => {
+//     console.log("Old frame:", event.target);
+//     console.log("New frame:", event.detail.newFrame);
+//     console.log("Old frame elements:", event.target.querySelectorAll('[data-turbo-transition]'));
+//     console.log("New frame elements:", event.detail.newFrame.querySelectorAll('[data-turbo-transition]'));
+
+//     if (shouldPerformTransition()) {
+//         event.preventDefault();
+//         debugger
+//         performTransition(
+//             event.target,
+//             event.detail.newFrame,
+//             async () => {
+//                 await event.detail.resume();
+//             }
+//         );
+//     }
+//   });
